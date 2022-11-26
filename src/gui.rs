0f40060c7cb4283871a58c1bb90c602313fb4806
maxstrid <maxstrid@github.io>
2022-11-26 09:26:39 -0800

    Refacter the gui structure

--- src/gui.rs
+++ src/gui.rs
@@ -1,58 +1,108 @@
-use gtk::{prelude::*, ScrolledWindow};
-use gtk::{Application, ApplicationWindow};
-use gtk::{TextBuffer, TextView};
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Label, ScrolledWindow, TextBuffer, TextView, FileChooserNative, Button};
 
 use crate::markdown::{read, write};
 
+const DEFAULT_WINDOW_WIDTH: i32 = 800;
+const DEFAULT_WINDOW_HEIGHT: i32 = 600;
+
+#[derive(Clone)]
 pub struct Gui<'a> {
     app: &'a Application,
-    filename: Option<String>,
-    file_data: Option<String>,
+    window: ApplicationWindow,
+    text_buffer: TextBuffer,
+    files: Option<Vec<(String, bool)>> // (Filename, is the file open)
 }
 
 impl<'a> Gui<'a> {
     pub fn new(app: &'a Application) -> Self {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("Markdown Editor")
+            .width_request(DEFAULT_WINDOW_WIDTH)
+            .height_request(DEFAULT_WINDOW_HEIGHT)
+            .build();
+        
+        let text_buffer = TextBuffer::builder()
+            .enable_undo(true)
+            .build();
+
         Self {
             app,
-            filename: None,
-            file_data: None,
+            window,
+            text_buffer,
+            files: None,
         }
     }
 
     pub fn present(&mut self) {
         // Temporary read
-        let data = read("example.md".to_string()).unwrap();
-
-        let title = match &self.filename {
-            Some(filename) => format!("Markdown Editor {filename}"),
-            None => "Markdown Editor".to_string(),
+        let data = match read("example.md".to_string()) {
+            Ok(data) => {
+                if self.files.is_none() {
+                    let _files: Vec<(String, bool)> = vec![("example.md".to_string(), true)];
+                    self.files = Some(_files);
+                }
+                data
+            },
+            Err(err) => {
+                self.error_window(err.as_str());
+                "".to_string()
+            }
         };
 
-        let window = ApplicationWindow::builder()
-            .application(self.app)
-            .title(title.as_ref())
-            .build();
+        self.text_buffer.set_text(data.as_str());
 
-        let buffer = TextBuffer::builder()
-            .enable_undo(true)
-            .text(data.as_ref())
-            .build();
+        self.build_ui();
 
-        buffer.connect_insert_text(|itself, iter, stri| {
-            println!(
-                "{}",
-                itself.text(&itself.start_iter(), &itself.end_iter(), false)
-            );
+        self.window.present();
+    }
+
+    fn build_ui(&self) {
+        // Top bar
+        let open_file_preview = FileChooserNative::builder()
+            .title("Choose A File")
+            .action(gtk::FileChooserAction::Open)
+            .transient_for(&self.window)
+            .accept_label("Open")
+            .cancel_label("Cancel")
+            .build();
+        
+        let open_file_button = Button::with_label("Open File");
+
+        open_file_button.connect_clicked(|_| {
+            todo!() // Do something with open_file_preview
         });
 
-        let view = TextView::builder()
+        // Text view
+        let text_view = TextView::builder()
+            .buffer(&self.text_buffer)
+            .editable(true)
             .accepts_tab(true)
-            .buffer(&buffer)
+            .cursor_visible(true)
+            .monospace(true)
+            .input_purpose(gtk::InputPurpose::FreeForm)
+            .wrap_mode(gtk::WrapMode::WordChar)
+            .build();
+        
+        let scrollable_view = ScrolledWindow::builder()
+            .child(&text_view)
+            .overlay_scrolling(true)
             .build();
 
-        let win = ScrolledWindow::builder().child(&view).build();
+        self.window.set_child(Some(&scrollable_view));
+    }
 
-        window.set_child(Some(&win));
-        window.present();
+    fn error_window(&self, error: &str) {
+        let error_window = ApplicationWindow::builder()
+            .application(self.app)
+            .title("Error!")
+            .width_request(600)
+            .height_request(200)
+            .build();
+
+        error_window.set_child(Some(&Label::new(Some(error))));
+
+        error_window.present();
     }
 }
